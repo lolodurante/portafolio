23de910461cda0638f937a080130504279a37867
Lorenzo Durante <lolodurante2003@gmail.com>
2023-01-06 21:48:40 -0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Work_Sans } from '@next/font/google'
+
+const workSans = Work_Sans({ subsets: ['latin'] })
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <div className={workSans.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
